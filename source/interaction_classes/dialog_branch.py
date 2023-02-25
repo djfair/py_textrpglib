5707eaafa088e1608e1bbfd8de4d0294c6033c6a
djfair <126334138+djfair@users.noreply.github.com>
2023-02-25 17:23:52 +0000

    Americanized filenames

---
+++ source/interaction_classes/dialog_branch.py
@@ -0,0 +1,27 @@
+from typing import Callable
+
+
+class DialogBranch:
+    """Class for carrying"""
+
+    def __init__(
+        self,
+        message: str,
+        options: dict[int, str],
+        outcomes: dict[int, tuple[Callable]],
+    ):
+        self.message = message
+        self.options = options
+        self.outcomes = outcomes
+
+    def __call__(self):
+        print(self.message)
+        for i, option in enumerate(self.options):
+            print(f"\t{i + 1}. {option}")
+        chosen_option = (
+            int(input(f"\t> What you want to do? [1-{len(self.options) + 1}]: ")) - 1
+        )
+        chosen_option = int(chosen_option) - 1
+        chosen_outcomes = self.outcomes[chosen_option]
+        for outcome in chosen_outcomes:
+            outcome()
